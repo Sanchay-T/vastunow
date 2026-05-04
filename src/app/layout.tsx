e5b8918aa756c703fc4c24d3f56b2e6f825d849a
Poojan <poojan.vig15991@sakec.ac.in>
2026-05-04 17:19:58 +0530

    data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Montserrat, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ClientProviders from "./providers";
 
@@ -32,6 +33,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wlto1f2a94");
+          `}
+        </Script>
+      </head>
       <body className={`${montserrat.variable} ${playfairDisplay.variable} antialiased bg-[var(--background)] text-[var(--foreground)]`}>
         <ClientProviders>
           {children}
